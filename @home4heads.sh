a1134f720555de7e28ad3ff60fbd67d25b88b772
Alexander Hutorezki <olekhy@users.noreply.github.com>
2021-01-31 11:50:02 +0100

    Initial minimal changes in config
    - Rofi
    - Jetbrains Font
    - Monitors setup for Home
    - i3 status bar
    - Tray icons
    - Screenshot via scrot

---
+++ @home4heads.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS-0 --off --output DP-0 --off --output DP-1 --mode 1680x1050 --pos 0x789 --rotate normal --output DP-2 --mode 1920x1080 --pos 1680x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off --output LVDS-1-1 --primary --mode 1920x1080 --pos 1680x1080 --rotate normal --output VGA-1-1 --mode 1680x1050 --pos 3600x30 --rotate normal
---
+++ README.md
@@ -0,0 +1,23 @@
+# i3-wm on Ubuntu 20.04
+
+  
+##### Packets are necessary to be installed
+
+```
+arandr           \
+i3lock           \
+i3lock-fancy     \
+lxappearance     \
+rofi             \
+scrot            \
+sudo apt install \
+viewnior         \
+volumeicon-alsa
+```
+
+##### Packets are optional to be installed
+
+```
+sudo apt install \
+i3blocks
+```
